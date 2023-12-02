3ccf9f61c392751ea8a9ea72ac4b1e552c9397a7
GtOliv3r <123522878+GtOliv3r@users.noreply.github.com>
2023-12-01 22:03:33 -0300

    chore:add all icons for use in development add 6 icons images for use in screens development

--- hydrosonar/app/(tabs)/index.tsx
+++ hydrosonar/app/(tabs)/index.tsx
@@ -1,31 +1,33 @@
-import { StyleSheet } from 'react-native';
+// src/screens/HomeScreen.tsx
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import BoxInfo from '../../components/AlertBox';
 
-import EditScreenInfo from '../../components/EditScreenInfo';
-import { Text, View } from '../../components/Themed';
-
-export default function TabOneScreen() {
+const HomeScreen: React.FC = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <BoxInfo
+        title="Título do Box"
+        description="Descrição do Box com informações adicionais."
+      />
+      <BoxInfo
+        title="Título do Box"
+        description="Descrição do Box com informações adicionais."
+      />
     </View>
+
+    
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
     justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+    alignItems: 'center',
+    backgroundColor: '#ecf0f1',
+    padding: 8,
   },
 });
+
+export default HomeScreen;
