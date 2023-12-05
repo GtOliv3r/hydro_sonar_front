394ba37388bc09cb5637269e20a0a9cb222dcf70
RafaMattss <rafael.mattss@gmail.com>
2023-12-04 21:45:22 -0300

    feat: home page

--- hydrosonar/app/(tabs)/index.tsx
+++ hydrosonar/app/(tabs)/index.tsx
@@ -2,18 +2,14 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import BoxInfo from '../../components/AlertBox';
+import NivelAgua from '../../components/NivelAgua';
+import EstadoValvula from '../../components/EstadoValvula';
 
 const HomeScreen: React.FC = () => {
   return (
     <View style={styles.container}>
-      <BoxInfo
-        title="Título do Box"
-        description="Descrição do Box com informações adicionais."
-      />
-      <BoxInfo
-        title="Título do Box"
-        description="Descrição do Box com informações adicionais."
-      />
+      <NivelAgua percentagem={60} litrosRestantes={10} />
+      <EstadoValvula estado={true} />
     </View>
 
     
