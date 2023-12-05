a412fd85a9a3980426435b5c9b648172f97db962
RafaMattss <rafael.mattss@gmail.com>
2023-12-04 21:44:56 -0300

    feat:alert box

--- hydrosonar/components/AlertBox.tsx
+++ hydrosonar/components/AlertBox.tsx
@@ -1,22 +1,43 @@
-// components/BoxInfo.tsx
 import React from 'react';
 import { View, Text, Image, StyleSheet } from 'react-native';
 
-// Importe a imagem localmente
-import ImagemLocal from '../assets/images/alerta.png';
-
 interface BoxInfoProps {
-  title: string;
-  description: string;
+  percentagem: number;
 }
 
-const BoxInfo: React.FC<BoxInfoProps> = ({ title, description }) => {
+const BoxInfo: React.FC<BoxInfoProps> = ({ percentagem }) => {
+  // Lógica para determinar se o BoxInfo deve ser renderizado
+  const shouldRenderBoxInfo = percentagem <= 20 || percentagem >= 80;
+
+  if (!shouldRenderBoxInfo) {
+    // Se não deve ser renderizado, retornamos nulo (nada será renderizado)
+    return null;
+  }
+
+  // Títulos e mensagens com base na porcentagem
+  let title = '';
+  let description = '';
+
+  if (percentagem >= 80 && percentagem < 90) {
+    title = 'Nível alto';
+    description = 'O nível de água está acima de 80%';
+  } else if (percentagem >= 90) {
+    title = 'Nível muito alto';
+    description = 'O nível de água está acima de 90%';
+  } else if (percentagem <= 20 && percentagem > 11) {
+    title = 'Nível baixo';
+    description = 'O nível de água está abaixo de 20%';
+  } else if (percentagem <= 10) {
+    title = 'Nível muito baixo';
+    description = 'O nível de água está abaixo de 10%';
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>{title}</Text>
       <View style={styles.content}>
         {/* Utilize a imagem importada localmente */}
-        <Image source={ImagemLocal} style={styles.image} />
+        <Image source={require('../assets/images/alerta.png')} style={styles.image} />
         <View style={styles.textContainer}>
           <Text style={styles.description}>{description}</Text>
         </View>
@@ -38,8 +59,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   image: {
-    width: 50,
-    height: 50,
+    width: 40,
+    height: 40,
     marginRight: 16,
     alignSelf: 'flex-start',
   },
